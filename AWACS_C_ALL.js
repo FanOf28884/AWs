e1b7ac666fcf1dd99939271ebc71c737d83b1e17
take4123 <52430717+take4123@users.noreply.github.com>
2019-09-07 10:25:56 +0900

    Update AWACS_C_ALL.js

--- AWACS_C_ALL.js
+++ AWACS_C_ALL.js
@@ -1,4 +1,4 @@
-function ______common______(){} 
+function ______common______(){}
 $(function(){if(typeof(dropZone)!='undefined')return;var $d=$('body');$d.on("dragenter",function(e){e.stopPropagation();e.preventDefault();});$d.on("dragover",function(e){e.stopPropagation();e.preventDefault();});$d.on("drop",function(e){e.stopPropagation();e.preventDefault();var files=e.originalEvent.dataTransfer.files;for(var i=0;i<files.length;i++){var f=files[i];fname=f.name;if(fname.slice(-4)=='.csv'||fname.slice(-3)=='.js'||fname.slice(-5)=='.json'){var reader=new FileReader();reader.onload=function(theFile){var text=reader.result.trim();if(fname.slice(-4)=='.csv'){var cols=text.split('\n');var data=[];for(var r=0;r<cols.length;r++){data.push("["+cols[r]+"]");}
 text=fname.replace('.csv','')+'= ['+data.join(',')+']';}
 eval(text);};reader.readAsText(files[i]);}}});}());function CopyToClipboard(note){function listener(e){e.clipboardData.setData("text/html",note);e.clipboardData.setData("text/plain",note);e.preventDefault();}
@@ -220,3 +220,98 @@ if(isLast)setTimeout(function(){doPERSort('p');sortChked();doPERSort('r');sortCh
 function doPERSort(color){var txt=extractCode($("#txtCode").val());$("#txtCode").val(txt+' ');var codes=txt.split(' ');for(var i=0;i<codes.length;i++){var code=codes[i];var SC=stock[code];if(SC.Color==color)
 document.getElementById(code).parentNode.children[0].checked=true;else
 document.getElementById(code).parentNode.children[0].checked=false;}}
+//--------AWACS_proto---------------------------------------------------------
+var i, SC;
+ChartSB.prototype = {
+    getCode: function () {
+        return this.code;
+    },
+    clearZone: function (n) {
+        this.ctx.fillStyle = '#FFFFFF';
+        this.ctx.fillRect(this.gl, this.g1d - 5 + 7 * n, this.lastX - this.gl, 7);
+        return this;
+    },
+    fillZone: function (n, color, i) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(this.lastX - i, this.g1d - 5 + 7 * n, 3, 6);
+        return this;
+    },
+    drawAHL: function () {
+        for (var i = this.AHLu.length - 1; i >= 0; i--) {
+            if (this.Prh[i] <= this.AHLu[i])
+                this.fillZone_1('#4040FF', i);
+            if (this.Prl[i] >= this.AHLd[i])
+                this.fillZone_1('#FF4040', i);
+        }
+        return this;
+    },
+    B3mTouch: function () {
+        var B3u = this.B3u;
+        var B3d = this.B3d;
+        var Prh = this.Prh;
+        var Prl = this.Prl;
+        for (var i = B3u.length - 1; i >= 0; i--) {
+            var ma = (B3u[i] + B3d[i]) / 2;
+            if (Prh[i] >= ma && ma >= Prl[i])
+                this.fillZone_1('black', i);
+        }
+        return this;
+    },
+    bgn: function () {
+        this.result = true;
+        return this;
+    },
+    end: function () {
+        return this.result;
+    },
+    oBOT: function () {
+        this.result = this.RCI[i] >= 80;
+        return this;
+    },
+    oBOT: function () {
+        this.result = this.RCI[i] <= -80;
+        return this;
+    },
+    PrGT: function (val) {
+        this.result = this.Prh[i] > val;
+        return this;
+    },
+    PrLT: function (val) {
+        this.result = this.Prl[i] < val;
+        return this;
+    },
+    DiffMaLT: function (val) {
+        var ma = (this.B3u[i] + this.B3d[i]) / 2;
+        var hl = (this.Prh[i] + this.Prl[i]) / 2
+        this.result = Math.abs(hl - ma) / ma * 100 < val;
+        return this;
+    },
+    GCros: function () {
+        this.result = this.GcDc[i] == 1;
+        return this;
+    },
+    DCros: function () {
+        this.result = this.GcDc[i] == -1;
+        return this;
+    },
+    DCuTouch: function () {
+        this.result = this.DCu[i] == this.Prh[i];
+        return this;
+    },
+    DCdTouch: function () {
+        this.result = this.DCd[i] == this.Prl[i];
+        return this;
+    },
+}
+function test(code) {
+    SC = stock[code];
+    SC.clearZone(1);
+    SC.tmp = [];
+    for (i = 0; i < SC.RCI.length; i++) {
+        SC.tmp[i] = SC.bgn().DiffMaLT(0.1).end();
+    }
+    for (i = 0; i < SC.RCI.length; i++) {
+        if (SC.tmp[i])
+            SC.fillZone(1, 'black', i);
+    }
+}
